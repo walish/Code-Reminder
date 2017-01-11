d97c94cf95eef4742383e1eb9883b8fe135e07f1
Walish Le <lehoangquan605@gmail.com>
2017-01-11 14:59:46 +0700

    Create php7_install.sh

---
+++ php7_install.sh
@@ -0,0 +1,20 @@
+sudo apt-get install python-software-properties 
+
+# With Ubuntu version <= 16.04, we need to add PPA for PHP7
+# sudo add-apt-repository ppa:ondrej/php
+
+sudo apt-get update
+
+sudo apt-get install -y php7.0
+# Or 
+# sudo apt-get install -y php7.1
+
+
+# Search for PHP7 modules
+# sudo apt-cache search php7-*
+
+# Get php version, e.g: 7.0 | 7.1
+PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
+
+sudo apt-get install php${PHP_VERSION}-mysql php${PHP_VERSION}-curl php${PHP_VERSION}-xml php${PHP_VERSION}-mcrypt 
+php${PHP_VERSION}-intl
