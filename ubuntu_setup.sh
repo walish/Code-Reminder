18e31145a50a677695fbe1d24e43d547e937aecc
Walish Le <lehoangquan605@gmail.com>
2017-01-18 12:59:39 +0700

    Create ubuntu_setup.sh

---
+++ ubuntu_setup.sh
@@ -0,0 +1,6 @@
+# Terminator
+echo '\033[92m start install terminator \033[0m'
+sudo add-apt-repository ppa:gnome-terminator -y 
+sudo apt-get update 
+sudo apt-get install terminator -y
+echo '\033[92m complete install terminator \033[0m'
