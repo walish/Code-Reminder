9ad51e5d6cf5d3541ae2d3ca36a8bc7a079aaedb
Walish Le <lehoangquan605@gmail.com>
2017-03-09 09:18:20 +0700

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4,3 +4,12 @@ sudo add-apt-repository ppa:gnome-terminator -y
 sudo apt-get update 
 sudo apt-get install terminator -y
 echo '\033[92m complete install terminator \033[0m'
+
+# Skype for Linux
+echo '\033[92m start install Skype for Linux \033[0m'
+sudo apt install apt-transport-https
+curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
+echo "deb https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
+sudo apt update
+sudo apt install skypeforlinux
+echo '\033[92m complete install Skype for Linux \033[0m'
